bb6f3c6e32d13ed4efbb943d11fc9f94861d94c4
Nabin_Neupane <157932924+nabinnew@users.noreply.github.com>
2026-04-18 14:54:12 +0545

    Change MongoDB connection to a specific URI
    Updated MongoDB connection string to use a specific URI.

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI); // no extra options
+    await mongoose.connect("mongodb+srv://parvatneupane104_db_user:JLGFCcnIFLlzkZPK@cluster0.jjxlbvf.mongodb.net/?appName=Cluster0"); // no extra options
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err.message);
